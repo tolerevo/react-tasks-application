54ec185c92e5131451af5029e692e96ff52f7cad
tolerevo <tolerevo@hotmail.com>
2024-01-17 13:35:23 -0600

    finish rendering tareas

---
+++ src/TaskList.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { tasks as data } from "./tasks.js";
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  if (tasks.length === 0) {
+    return <h1>No hay tareas aún</h1>;
+  }
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
